3caa1761da4ed17dc89c6555df5dbc4642480b8a
Mothermime <mothermime@gmail.com>
2016-11-02 16:54:42 +1300

    Implement saving a market

--- MarketPlaces/Controllers/MarketsController.cs
+++ MarketPlaces/Controllers/MarketsController.cs
@@ -1,7 +1,9 @@
-﻿using MarketPlaces.Models;
+﻿using System;
+using MarketPlaces.Models;
 using MarketPlaces.ViewModels;
 using System.Linq;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace MarketPlaces.Controllers
 {
@@ -14,13 +16,47 @@ namespace MarketPlaces.Controllers
             _context = new ApplicationDbContext();
         }
 
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new MarketFormViewModel
             {
                 Categories = _context.Categories.ToList()
             };
+
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public ActionResult Create(MarketFormViewModel viewModel)
+
+        {
+            {
+
+
+                //if (!ModelState.IsValid)
+                //{
+                //    viewModel.Categories = _context.Categories.ToList();
+                //}
+
+                //return View("Create", viewModel);
+
+
+                var market = new Market
+                {
+                    OrganiserId = User.Identity.GetUserId(),
+                    CategoryId = viewModel.Category,
+                    DateTime = viewModel.GetDateTime(),
+                    Title = viewModel.Title,
+                    Venue = viewModel.Venue
+
+                };
+                _context.Markets.Add(market);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
         }
     }
 }
